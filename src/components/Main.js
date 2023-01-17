0a809ee34d1725fa2af980d792a8ff3c14bf94fb
Reiny Botros <reinybotros@Reinys-MacBook-Pro.local>
2023-01-17 08:11:56 -0800

    attempt at create new

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,37 +5,52 @@ import Index from '../pages/Index';
 import ItemShow from '../pages/ItemShow';
 import Wanted from '../pages/Wanted';
 
-function Main() {
+function Main(props) {
 
   const [items, setItems] = useState(null)
   const URL= "http://mercantile.herokuapp.com/"
 
-  const getItems = async() =>{
-    const response = await fetch(URL+"items");
-    const data = await response.json()
-      setItems(data)
-      }
-      useEffect(()=>{
-      getItems();
-      },[])
+  const getItems = async() => {
+    const response = await fetch(URL+'items');
+    const data = await response.json();
+    setItems(data);
+  }
+
+  const createItem = async(item) => {
+    //make post request to create item
+    await fetch(URL + "items", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json'
+      },
+      body: JSON.stringify(item),
+    });
+    //update list of people
+    getItems();
+  }
+
+  useEffect(()=>{
+    getItems();
+  },[])
 
      
-      return (
-        <main>
-          <Routes>
-            <Route 
-              path="/" 
-              element={<Index items={items}/>} 
-            />
+  return (
+    <main>
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Index items={items} createItem={createItem}/>} 
+        />
 
-            <Route 
-            path="/items/:id"
-            element={<ItemShow />}/> 
+        <Route 
+          path="/items/:id"
+          element={<ItemShow />}
+        /> 
 
-            <Route path="/wanted" element={<Wanted />}/>
-          </Routes>
-        </main>
-      );
-    }
+        <Route path="/wanted" element={<Wanted />}/>
+      </Routes>
+    </main>
+  );
+}
     
-    export default Main;
+export default Main;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,16 +1,73 @@
 import React from 'react'
 import Item from '../components/Item'
-function Index(props) {
-  console.log(props)
-let frontPageItems=[]
-for (let item of props.items){
+import {useState} from'react';
+import {Link} from 'react-router-dom'
 
-  frontPageItems.push(<Item item={item}/>)
-}
+function Index(props) {
+
+  const formFields = {
+    name: '',
+    condition: '',
+  };
+
+  //state to hold formData
+  const [newForm, setNewForm] = useState(formFields);
+
+  //handleChange function for form
+  const handleChange = (event) => {
+    setNewForm({...newForm, [event.target.name]: event.target.value})
+  };
+
+  //handleSubmit function for form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createItem(newForm);
+    setNewForm(formFields)
+  };
+
+  //loaded function
+  const loaded = () => {
+    return props.items.map(item => (
+      <div key={item._id}>
+        <Link to={`/items/${item._id}`}>
+          <h1>{item.name}</h1>
+        </Link>
+      </div>
+    ));
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>
+  };
+
+  console.log(props)
+  let frontPageItems=[]
+  // for (let item of props.items){
+
+  //   frontPageItems.push(<Item item={item}/>)
+  // }
 
   return (
     <div className='container'>
       {frontPageItems}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={newForm.name}
+          name='name'
+          placeholder='name'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          value={newForm.condition}
+          name='condition'
+          placeholder='condition'
+          onChange={handleChange}
+        />
+        <input type='submit' value='create item' />
+      </form>
+      {props.people ? loaded() :loading()}
     </div>
   )
 }
